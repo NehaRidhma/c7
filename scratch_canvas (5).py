4b9ae665a23be12166f79d7bd9ee09b261027e8f
NehaRidhma <94739547+NehaRidhma@users.noreply.github.com>
2021-12-23 12:45:41 +0530

    Add files via upload

---
+++ scratch_canvas (5).py
@@ -0,0 +1,15 @@
+def odd_even(n):
+    if n % 2 == 0:
+        print(n ,"is an even number")
+    else:
+        print(n ,"is an odd number")
+        
+      
+    
+    
+odd_even(60)
+odd_even(99)
+odd_even(98)
+odd_even(45)
+        
+   
